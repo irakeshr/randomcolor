7daaf4f5b093fbf34dd548b001af895a736c0628
irakeshr <iamrakeshr0@gmail.com>
2023-05-28 12:46:14 +0530

    some changes

--- random.js
+++ random.js
@@ -1,5 +1,4 @@
-let num1=Math.random();
-let body=document.querySelector("body");
+
 let colors= document.querySelector("h1");
 let box= document.querySelector(".box");
 let main= document.querySelector(".main");
@@ -10,10 +9,7 @@ let but= document.querySelector("button");
 let hesh="#";
 let color="1234567890ABCDEF";
 for(i=0; i<6; i++){
-
     hesh+=color[Math.floor(Math.random()*16)]  ;
-    console.log(hesh);
-
 }
 colors.innerHTML=hesh;
 main.style.background=hesh;
