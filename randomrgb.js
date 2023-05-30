634509c12e1f745700b7bb1abc7509733e8fc454
irakeshr <iamrakeshr0@gmail.com>
2023-05-30 11:32:30 +0530

    add rgb color picker and loader screen

--- randomrgb.js
+++ randomrgb.js
@@ -1,4 +1,3 @@
-
 let colors= document.querySelector("h1");
 let box= document.querySelector(".box");
 let main= document.querySelector(".main");
@@ -6,27 +5,34 @@ let but= document.querySelector(".button");
 let page_switcher=document.querySelector(".page_switcher");
 let loader=document.querySelector(".loader");
  
+ 
  function onclick1(){
-let hesh="#";
-let color="1234567890ABCDEF";
-for(i=0; i<6; i++){
-    hesh+=color[Math.floor(Math.random()*16)]  ;
+let hesh="rgb(";
+let color="1234567890";
+for(i=0; i<3; i++){
+    hesh+=color[Math.floor(Math.random()*10)]  ;
 }
+hesh+=",";
+for(i=0; i<3; i++){
+    hesh+=color[Math.floor(Math.random()*10)]  ;
+}
+hesh+=",";
+for(i=0; i<3; i++){
+    hesh+=color[Math.floor(Math.random()*10)]  ;
+}
+hesh+=")"
+
 colors.innerHTML=hesh;
 main.style.background=hesh;
 box.style.background=hesh;
 but.style.background=hesh;
+ 
    
  };
    onclick1();
 //    button event
   but.addEventListener("click",onclick1);
 //   loader.style.display="none";
-
-  page_switcher.onclick =()=>{
-    loader.style.display="flex";
-
-  }
 
   window.addEventListener("load",function(){
     loader.style.display="none";
